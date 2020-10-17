fc310e06ab3d121891a6ecdb7336a9c153841a25
Anurag Rallabandi <54565746+anuragralla@users.noreply.github.com>
2020-10-17 14:23:40 -0400

    Add files via upload

---
+++ blog/config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  title: 'My Blog', // Required
+  author: 'Anurag Rallabandi', // Required
+  description: 'Machine Learning {Enthusiast, Researcher, App Developer}',
+  primaryColor: '#000000', // Required
+  showHeaderImage: true,
+  showShareButtons: true,
+  postsPerPage: 5, // Required
+  social: {
+    website: 'https://anuragrall.github.io',
+    github: 'https://github.com/anuragralla',
+    linkedin: 'https://https://www.linkedin.com/in/anurag-rallabandi-3b1562136/',
+  },
+  pathPrefix: '/blog',
+  siteUrl: 'https://anuragralla.github.io/blog/',
+};
